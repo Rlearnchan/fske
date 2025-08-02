5c37fe1aba66c39dcf62fdd5bf529f3f8dff37af
Hyeongchan Bae <83401838+Rlearnchan@users.noreply.github.com>
2025-08-02 21:18:44 +0900

    250802

--- setup.sh
+++ setup.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 set -e
-
-# 가상환경 생성
 python3 -m venv fske-venv
 source fske-venv/bin/activate
-
-# pip 업그레이드 및 필수 휠 설치
 pip install --upgrade pip wheel
-
-# requirements 설치
-pip install -r /fske/requirements.txt
-
-# ipykernel 커널 등록 (필요 시)
-python -m ipykernel install --user --name=fske-venv --display-name "Python (fske)"
+pip install -r requirements.txt
